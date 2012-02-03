58a90dd1c6951e415ad11e85565d7a70625b321c
Dan Kubb <dan.kubb@gmail.com>
2012-02-03 00:02:29 -0800

    [4] Reverting to an earlier cleaner version

--- 004/brute_force.hs
+++ 004/brute_force.hs
@@ -1,9 +1,7 @@
-import Data.List (sortBy)
-
 main = print answer
 
 isPalindrome n =
   let string = show n
   in string == reverse string
 
-answer = head $ filter isPalindrome $ sortBy (flip compare) [ n * m | n <- [100..999], m <- [n..999] ]
+answer = maximum [ x | n <- [100..999], m <- [n..999], let x = n*m, isPalindrome x ]
