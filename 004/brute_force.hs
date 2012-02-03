154896540835572f252dc335b475e691fcff6b43
Dan Kubb <dan.kubb@gmail.com>
2012-02-02 21:42:01 -0800

    Remove unecessary whitespace

--- 004/brute_force.hs
+++ 004/brute_force.hs
@@ -4,4 +4,4 @@ isPalindrome n =
   let string = show n
   in string == reverse string
 
-answer = maximum $ filter isPalindrome [ n * m | n <- [100..999], m <- [100..999]]
+answer = maximum $ filter isPalindrome [n*m | n <- [100..999], m <- [100..999]]
