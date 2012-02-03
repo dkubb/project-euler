aa9d12f17c549a36405ce0e5b8fe9e88982ec63c
Dan Kubb <dan.kubb@gmail.com>
2012-02-02 21:39:06 -0800

    Simplified brute force method
    * No point (yet) in counting down from the highest numbers; at least not until I
      can figure out a more efficient algo for this.

--- 004/brute_force.hs
+++ 004/brute_force.hs
@@ -4,4 +4,4 @@ isPalindrome n =
   let string = show n
   in string == reverse string
 
-answer = maximum $ filter isPalindrome [ n * m | n <- [999, 998..100], m <- [999, 998..100]]
+answer = maximum $ filter isPalindrome [ n * m | n <- [100..999], m <- [100..999]]
