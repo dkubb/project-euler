d7210013aa7d650b1a6f1f0204133dd0938375f9
Dan Kubb <dan.kubb@gmail.com>
2012-02-02 16:48:23 -0800

    Break up longer line into smaller lines for easier readability

--- 003/trial_division.hs
+++ 003/trial_division.hs
@@ -3,7 +3,9 @@ main = print answer
 factorize n = factorize' n 0
   where
     factorize' 1 f = f
-    factorize' n _ = let lm = lowest_multiple n in factorize' (div n lm) lm
+    factorize' n _ =
+      let lm = lowest_multiple n
+      in factorize' (div n lm) lm
 
 lowest_multiple n = head [f | f <- trial_factors n, divisible n f]
 trial_factors n   = [2..]
