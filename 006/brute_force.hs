1c97afa636d648baf160ab19eca40fc4e2c175e3
Dan Kubb <dan.kubb@gmail.com>
2012-02-03 20:13:11 -0800

    [6] Updated brute force to be simpler
    * The operations are actually so simple they don't need to be in their own
      functions (for this question anyway)

--- 006/brute_force.hs
+++ 006/brute_force.hs
@@ -1,6 +1,3 @@
 main = print answer
 
-sumOfSquares = sum . map (^2)
-squareOfSums = (^2) . sum
-
-answer = squareOfSums [1..100] - sumOfSquares [1..100]
+answer = sum [1..100]^2 - (sum $ map (^2) [1..100])
